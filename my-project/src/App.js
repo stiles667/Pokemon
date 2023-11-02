875a276390dc8aceaf4076caaeed0ca11fa940a1
stiles667 <i.elyamani@ecole-ipssi.net>
2023-11-02 11:35:55 +0100

    update
    commit*

---
+++ my-project/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import Api from './composant/Main';
+import Routes from
+function App() {
+  return (
+    <div className="App">
+     r
+    </div>
+  );
+}
+
+export default App;
---
+++ my-project/src/composant/Main.jsx
@@ -0,0 +1,22 @@
+import React, { useState, useEffect } from 'react';
+
+export default function Accueil() {
+    const [pokemon, setPokemon] = useState([]);
+
+    useEffect(() => {
+        fetch('https://pokeapi.co/api/v2/pokemon', {
+            method: "GET"
+        })
+        .then(response => response.json())
+        .then(data => setPokemon(data.results))
+        .catch(error => console.log(error));
+    }, []);
+    console.log(pokemon);
+    return (
+        <div>
+          <h1> {pokemon.data.name}</h1>
+          <img src={pokemon.data.} alt="" />
+
+        </div>
+    )
+}

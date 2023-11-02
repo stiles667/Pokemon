09fec010d1c188db878087bc797b4d8bc1034040
stiles667 <i.elyamani@ecole-ipssi.net>
2023-11-02 12:20:45 +0100

    update

--- my-project/src/App.js
+++ my-project/src/App.js
@@ -2,13 +2,12 @@ import './App.css';
 import Api from './composant/Main';
 import { BrowserRouter } from "react-router-dom";
 import { Link, Route, Routes } from "react-router-dom";
-import Header from './composant/Header';
 function App() {
   return (
 
     <div className="App">
-          <Link>
      <Api/>
+     <Link>
      </Link>
     </div>
   );

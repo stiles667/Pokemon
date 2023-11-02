8104a1f201b17a2dfdae23fe81040df796469fe3
stiles667 <i.elyamani@ecole-ipssi.net>
2023-11-02 14:10:30 +0100

    update

--- my-project/src/composant/Main.jsx
+++ my-project/src/composant/Main.jsx
@@ -1,36 +1,44 @@
-import React, { useState, useEffect } from 'react';
-import './Main.css';
+import React, { useState, useEffect } from "react";
+import "./Main.css";
 
 export default function Accueil() {
+  const [pokemon, setPokemon] = useState([]);
 
-
-    
-    const [pokemon, setPokemon] = useState([]);
-
-    useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon/4', {
-            method: "GET"
-        })
-        .then(response => response.json())
-        .then(data => setPokemon(data))
-        .catch(error => console.log(error));
-    }, []);
-    console.log(pokemon);
-    return (
-        <div>
-                <header><h1>Welcome to My POKEDEX</h1> 
-                <img src="" alt="" /></header>
-    <div className=' Infos'>
-          <h1> {pokemon.name}</h1>
-            <img src={pokemon.sprites?.front_default} alt={pokemon.name} />
-            <h2> {pokemon.weight}</h2>
-            <h2> {pokemon.height}</h2>
-            <h2> {pokemon.base_experience}</h2>
-            <h2> {pokemon.abilities?.map(ability => ability.ability.name).join(', ')}</h2>
-            </div>
-
-          
-
-        </div>
-    )
+  useEffect(() => {
+    fetch("https://pokeapi.co/api/v2/pokemon/4", {
+      method: "GET",
+    })
+      .then((response) => response.json())
+      .then((data) => setPokemon(data))
+      .catch((error) => console.log(error));
+  }, []);
+  console.log(pokemon);
+  return (
+    <div>
+      <header>
+        <h1 className="titre">Welcome to My POKEDEX</h1>
+        <img src="" alt="" />
+        <div className="topnav">
+        <a class="active" href="#home">
+          Home
+        </a>
+       
+        <input type="text" placeholder="Search.." />
+      </div>
+      </header>
+      <div className=" Infos">
+        <h1> {pokemon.name}</h1>
+        <img src={pokemon.sprites?.front_default} alt={pokemon.name} />
+        <h2> {pokemon.weight}</h2>
+        <h2> {pokemon.height}</h2>
+        <h2> {pokemon.base_experience}</h2>
+        <h2>
+          {" "}
+          {pokemon.abilities?.map((ability) => ability.ability.name).join(", ")}
+        </h2>
+        
+      </div>
+      
+    </div>
+  );
 }

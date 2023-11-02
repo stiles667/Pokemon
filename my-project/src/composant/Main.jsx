c86a4334cd2a19dc71956538cb046756581b76a3
stiles667 <i.elyamani@ecole-ipssi.net>
2023-11-02 11:47:48 +0100

    Update Main.jsx

--- my-project/src/composant/Main.jsx
+++ my-project/src/composant/Main.jsx
@@ -4,18 +4,19 @@ export default function Accueil() {
     const [pokemon, setPokemon] = useState([]);
 
     useEffect(() => {
-        fetch('https://pokeapi.co/api/v2/pokemon', {
+        fetch('https://pokeapi.co/api/v2/pokemon/6', {
             method: "GET"
         })
         .then(response => response.json())
-        .then(data => setPokemon(data.results))
+        .then(data => setPokemon(data))
         .catch(error => console.log(error));
     }, []);
     console.log(pokemon);
     return (
         <div>
-          <h1> {pokemon.data.name}</h1>
-          <img src={pokemon.data.sprites} alt="" />
+          <h1> {pokemon.name}</h1>
+            {/* <img src={pokemon.sprites?.front_default} alt={pokemon.name} /> */}
+          
 
         </div>
     )

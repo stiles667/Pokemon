e4187c8210a28945facb02b06d6e6fc33836c225
stiles667 <i.elyamani@ecole-ipssi.net>
2023-11-03 12:52:00 +0100

    Update Main.jsx

--- my-project/src/composant/Main.jsx
+++ my-project/src/composant/Main.jsx
@@ -72,12 +72,14 @@ const Main = () => {
       <div className="container">
 
         <div className="topnav">
+          <div className="Links">
             <Link to="/" className="link-button">
               Home
             </Link>
             <Link to="/Pokedex" className="link-button">
               Pokedex
             </Link>
+            </div>
          <img src="https://o.remove.bg/downloads/c599fbf4-9a22-494e-864e-2039c0f23ebd/Symbole-Pokemon-removebg-preview.png" alt=""  style={{height:"120px",}}/>
           <div className="link-buttons">
           </div>
